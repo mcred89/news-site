b89e550578f00bf4ccb15875646e9c7224dd3b9d
mac <seanmcilroy89@gmail.com>
2019-02-13 17:29:42 -0600

    super basic site working

--- news.py
+++ news.py
@@ -4,27 +4,27 @@ from utils import make_news_summary
 
 
 def get_ap(outputs):
-    outputs['news']['ap'] = [{}, {}]
+    outputs['news']['AP'] = [{}, {}]
     url = 'https://www.apnews.com'
     source = requests.get(url).text
     soup = BeautifulSoup(source, 'lxml')
 
     first_story = soup.find('a', class_="headline")
     outputs = make_news_summary(
-        outputs, 'ap', url + first_story['href'],
+        outputs, 'AP', url + first_story['href'],
         0, first_story.text)
 
     second_story_container = soup.find('div', class_="RelatedStory")
     second_story_link = second_story_container.a
     second_story_title = second_story_container.find('div', class_="headline")
     outputs = make_news_summary(
-        outputs, 'ap', url + second_story_link['href'],
+        outputs, 'AP', url + second_story_link['href'],
         1, second_story_title.text)
 
     return outputs
 
 def get_reuters(outputs):
-    outputs['news']['reuters'] = [{}, {}]
+    outputs['news']['Reuters'] = [{}, {}]
     url = 'https://www.reuters.com'
     source = requests.get(url).text
     soup = BeautifulSoup(source, 'lxml')
@@ -33,14 +33,14 @@ def get_reuters(outputs):
     first_story = first_story_container.find('h2', class_="story-title")
     print(url + first_story.a['href'])
     outputs = make_news_summary(
-        outputs, 'reuters', url + first_story.a['href'],
+        outputs, 'Reuters', url + first_story.a['href'],
         0, first_story.text)
 
     second_story_container = soup.find('div', class_="news-headline-list")
     second_story_link = second_story_container.find('a')
     second_story_title = second_story_container.find('h3', class_="story-title")
     outputs = make_news_summary(
-        outputs, 'reuters', url + second_story_link['href'],
+        outputs, 'Reuters', url + second_story_link['href'],
         1, second_story_title.text.strip())
 
     return outputs
