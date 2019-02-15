fb1a713c91b2ab541c9e7b4751cf0ac0933d53a6
Sean McIlroy <mac@localhost.localdomain>
2019-02-15 07:43:58 -0600

    moving utils into scrape

--- scrape.py
+++ scrape.py
@@ -1,6 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
-from utils import make_news_summary
+from  newspaper import Article
+
+def get_summary(url):
+    article = Article(url)
+    article.download()
+    article.parse()
+    article.nlp()
+    return article.summary
+
+def make_news_summary(outputs, site, link, index, headline):
+    outputs['news'][site][index]['headline'] = headline
+    outputs['news'][site][index]['link'] = link
+    outputs['news'][site][index]['summary'] = get_summary(link)
+    return outputs
 
 
 def get_ap(outputs):
